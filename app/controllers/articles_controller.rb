0d85f0d2c0286156d77810fbb1592edd8a6a36d7
umair-xprolabs <muhammad.umair@xprolabs.com>
2023-08-22 16:33:55 +0500

    Completed getting started document

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name:'ddd', password:'secret', except: [:index,:show]
   def index
     @articles=Article.all
   end
@@ -9,16 +10,31 @@ class ArticlesController < ApplicationController
     @article=Article.new
   end
   def create
-    @article=Article.new(title:'...',details:'...')
+    @article=Article.new(article_params)
     if @article.save
+      redirect_to @article, notice:'Article Created Successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def edit
+    @article=Article.find(params[:id])
+  end
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(article_params)
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
   end
+  def destroy
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
   private
   def article_params
-    params.require(:article).permit(:title,:details)
-
+    params.require(:article).permit(:title,:details, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
   root "articles#index"
-  resources :articles
-  # get "/articles/index"
+  resources :articles do
+    resources :comments
+  end
 
 end
